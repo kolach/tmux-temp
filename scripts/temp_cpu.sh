6459daedb39abd18fc3ee9604527fec4881bafb9
kolach <chikoald@gmail.com>
2019-02-26 23:17:03 -0500

    fix lint errors

--- scripts/temp_cpu.sh
+++ scripts/temp_cpu.sh
@@ -7,19 +7,21 @@ source "$CURRENT_DIR/helpers.sh"
 
 print_cpu_temp() {
   if command_exists "sensors"; then
-    local u=$1
-    local t=$(sensors | sed '/^[^Package]/d' | sed '/^\s*$/d' | tail -n 1 | awk '{a=$4} END {printf("%f", a)}')
-    if [ "$u" = "F" ]; then
-      t=$(celsius_to_fahrenheit "$t")
+    local units=$1
+    local temp
+    temp=$(sensors | sed '/^[^Package]/d' | sed '/^\s*$/d' | tail -n 1 | awk '{a=$4} END {printf("%f", a)}')
+    if [ "$units" = "F" ]; then
+      temp=$(celsius_to_fahrenheit "$temp")
     fi
-    printf "%3.0fº%s" "$t" "$u"
+    printf "%3.0fº%s" "$temp" "$units"
   else
     echo "no sensors found"
   fi
 }
 
 main() {
-  local units=$(get_tmux_option "@temp_units" "C")
+  local units
+  units=$(get_tmux_option "@temp_units" "C")
   print_cpu_temp "$units"
 }
 main
--- temp.tmux
+++ temp.tmux
@@ -6,7 +6,7 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/scripts/helpers.sh"
 
 temp_interpolation=(
-  "\#{temp_cpu}"
+  "\\#{temp_cpu}"
 )
 
 temp_commands=(
@@ -23,8 +23,13 @@ do_interpolation() {
 
 update_tmux_option() {
   local option=$1
-  local option_value=$(get_tmux_option "$option")
-  local new_option_value=$(do_interpolation "$option_value")
+
+  local option_value
+  option_value=$(get_tmux_option "$option")
+
+  local new_option_value
+  new_option_value=$(do_interpolation "$option_value")
+
   set_tmux_option "$option" "$new_option_value"
 }
 
