09f787c8b53d47057345db8eb5d62d600f4d8069
Krzysztof Romanowski <krzysztof.romanowski94@gmail.com>
2020-04-04 12:55:56 +0200

    Add Raspbian compatibility
    RPi distros usually provides vcgencmd command that can be used for
    getting CPU Temperature stats and many more.
    
    Signed-off-by: Krzysztof Romanowski <krzysztof.romanowski94@gmail.com>

--- scripts/temp_cpu.sh
+++ scripts/temp_cpu.sh
@@ -6,17 +6,25 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/helpers.sh"
 
 print_cpu_temp() {
+  local temp
+  local units=$1
+
+  # try with very common lm-sensors package
   if command_exists "sensors"; then
-    local units=$1
-    local temp
     temp=$(sensors | sed '/^[^Package]/d' | sed '/^\s*$/d' | tail -n 1 | awk '{a=$4} END {printf("%f", a)}')
-    if [ "$units" = "F" ]; then
-      temp=$(celsius_to_fahrenheit "$temp")
-    fi
-    printf "%3.0fº%s" "$temp" "$units"
+
+  # try if this is Raspberry Pi
+  elif command_exists "vcgencmd"; then
+    temp=$(vcgencmd measure_temp | tr -d -c 0-9.)
+
   else
     echo "no sensors found"
   fi
+
+  if [ "$units" = "F" ]; then
+    temp=$(celsius_to_fahrenheit "$temp")
+  fi
+  printf "%3.0fº%s" "$temp" "$units"
 }
 
 main() {
